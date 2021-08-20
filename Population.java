f0b3bd2ab0a37828c61e6ac0d8864d004857db51
m-steph <86425576+m-steph@users.noreply.github.com>
2021-08-20 11:50:59 -0600

    Create Population.java

---
+++ Population.java
@@ -0,0 +1,19 @@
+public class Population {
+    public static void main(String[] args) {
+        System.out.println(getInitialStartTimeForMoose1(1000));
+        System.out.println(getInitialStartTimeForMoose1(1000));
+    }
+    public static int getInitialStartTimeForMoose1(int currentMoosePop) {
+      return 0;
+
+    }
+    public static int getInitialStartTimeForMoose2(int currentMoosePop) {
+      return 0;
+    }
+    public static int getMoosePopulationWithWolves(int currentMoosePop, int currentWolfPopulation, int years) {
+        
+    }
+    public static int getStartingMoosePopulationWithWolves(int currentMoosePop, int currentWolfPopulation) {
+        
+    }
+}
